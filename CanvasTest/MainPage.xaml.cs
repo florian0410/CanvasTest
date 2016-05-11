78b66195149df4f9ec2535f8a57d67e46741e663
FlorianS <FlorianS@POLICEPROJECT>
2016-05-11 16:21:45 -0700

    Add project files.

---
+++ CanvasTest/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Input.Inking;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CanvasTest
+{
+    /// <summary>
+    /// Test d'un Canvas pour tablette
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const int minPenSize = 2;
+        const int penSizeIncrement = 2;
+        int penSize;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            penSize = minPenSize + penSizeIncrement * PenThickness.SelectedIndex;
+
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Red;
+            drawingAttributes.Size = new Size(penSize, penSize);
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen | Windows.UI.Core.CoreInputDeviceTypes.Touch;
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // rootPage = MainPage.Current;     Utilisé quand on passe de la main page à celle-ci
+            outputGrid.Width = Window.Current.Bounds.Width;
+            outputGrid.Height = Window.Current.Bounds.Height;
+            inkCanvas.Width = Window.Current.Bounds.Width;
+            inkCanvas.Height = Window.Current.Bounds.Height;
+        }
+
+        private void OnPenTypeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                InkDrawingAttributes drawingAttributes = inkCanvas.InkPresenter.CopyDefaultDrawingAttributes();
+                string value = ((ComboBoxItem)PenType.SelectedItem).Content.ToString();
+
+                if (value == "Ballpoint")
+                {
+                    drawingAttributes.Size = new Size(penSize, penSize);
+                    drawingAttributes.PenTip = PenTipShape.Circle;
+                    drawingAttributes.DrawAsHighlighter = false;
+                    drawingAttributes.PenTipTransform = System.Numerics.Matrix3x2.Identity;
+                }
+                else if (value == "Highlighter")
+                {
+                    // Make the pen rectangular for highlighter
+                    drawingAttributes.Size = new Size(penSize, penSize * 2);
+                    drawingAttributes.PenTip = PenTipShape.Rectangle;
+                    drawingAttributes.DrawAsHighlighter = true;
+                    drawingAttributes.PenTipTransform = System.Numerics.Matrix3x2.Identity;
+                }
+                if (value == "Calligraphy")
+                {
+                    drawingAttributes.Size = new Size(penSize, penSize * 2);
+                    drawingAttributes.PenTip = PenTipShape.Rectangle;
+                    drawingAttributes.DrawAsHighlighter = false;
+
+                    // Set a 45 degree rotation on the pen tip
+                    double radians = 45.0 * Math.PI / 180;
+                    drawingAttributes.PenTipTransform = System.Numerics.Matrix3x2.CreateRotation((float)radians);
+                }
+                inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            }
+        }
+
+        private void OnPenThicknessChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                InkDrawingAttributes drawingAttributes = inkCanvas.InkPresenter.CopyDefaultDrawingAttributes();
+                penSize = minPenSize + penSizeIncrement * PenThickness.SelectedIndex;
+                string value = ((ComboBoxItem)PenType.SelectedItem).Content.ToString();
+                if (value == "Highlighter" || value == "Calligraphy")
+                {
+                    // Make the pen tip rectangular for highlighter and calligraphy pen
+                    drawingAttributes.Size = new Size(penSize, penSize * 2);
+                }
+                else
+                {
+                    drawingAttributes.Size = new Size(penSize, penSize);
+                }
+                inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            }
+        }
+
+        private void OnPenColorChanged(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                InkDrawingAttributes drawingAttributes = inkCanvas.InkPresenter.CopyDefaultDrawingAttributes();
+
+                // Use button's background to set new pen's color
+                var btnSender = sender as Button;
+                var brush = btnSender.Background as SolidColorBrush;
+
+                drawingAttributes.Color = brush.Color;
+                inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            }
+        }
+
+        private void Eraser_CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Erasing;
+        }
+
+        private void Eraser_CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.InkPresenter.InputProcessingConfiguration.Mode = InkInputProcessingMode.Inking;
+        }
+
+    }
+}
